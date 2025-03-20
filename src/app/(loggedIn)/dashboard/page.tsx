376330b17cfd8c72ef8a800d59796cb407bd312b
saturnwillbemine <lewistrejon@gmail.com>
2025-03-20 10:43:38 -0500

    updated so it takes the new data

--- src/app/(loggedIn)/dashboard/page.tsx
+++ src/app/(loggedIn)/dashboard/page.tsx
@@ -1,18 +1,56 @@
 import Class from '@/app/components/ClassCard'
 import { SimpleGrid } from '@mantine/core';
 
+const sampleClassData = [
+  {
+    className: 'Class 1',
+    deptName: 'C1',
+    classDesc: 'This is class 1',
+    classID: 10
+  },
+  {
+    className: 'Class 2',
+    deptName: 'C2',
+    classDesc: 'This is class 2',
+    classID: 20
+  },
+  {
+    className: 'Class 3',
+    deptName: 'C3',
+    classDesc: 'This is class 3',
+    classID: 30
+  },
+  {
+    className: 'Class 4',
+    deptName: 'C4',
+    classDesc: 'This is class 4',
+    classID: 40
+  },
+  {
+    className: 'Class 5',
+    deptName: 'C5',
+    classDesc: 'This is class 5',
+    classID: 50
+  },
+]
+
+ const classCardGrid = sampleClassData.map((card, index) => (
+    <div key={index}>
+      <Class
+        className={card.className}
+        deptName={card.deptName}
+        classDesc={card.classDesc}
+        classID={card.classID}
+      />
+    </div>
+    ));
+
 
 export default function Dashboard() {
   return (
     <>
     <SimpleGrid cols={4} spacing='sm'>
-      <div><Class/></div>
-      <div><Class/></div>
-      <div><Class/></div>
-      <div><Class/></div>
-      <div><Class/></div>
-      <div><Class/></div>
-      <div><Class/></div>
+      {classCardGrid}
     </SimpleGrid>
     </>
   );
