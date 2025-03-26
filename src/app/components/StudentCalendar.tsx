949832fe3566c0fada6ce46381d5a75da90206e7
saturnwillbemine <lewistrejon@gmail.com>
2025-03-26 10:07:39 -0500

    fixed calendar and added comments

--- src/app/components/StudentCalendar.tsx
+++ src/app/components/StudentCalendar.tsx
@@ -8,15 +8,15 @@ export default function StudentCalendar() {
   const dateRenderer: DatePickerProps['renderDay'] = (Date) => {
     const day = Date.getDate();
     return(
-        <Indicator size={6} color='red' offset={-5} disabled={ day !== 1 }>
+        <Indicator size={6} color='red' offset={-5} disabled={ day !== 1 }> 
             <div>{day}</div>
         </Indicator>
-    );
+    ); // this gives us the indicator over the day of the month for every day not in the disabled prop, if its not in the array we get back we set it to disabled
   };
 
   return (
     <div>
-        <Calendar static renderDay={dateRenderer}/>
+        <Calendar maxLevel='month' minLevel='month' static renderDay={dateRenderer}/>
     </div>
   );
 }
