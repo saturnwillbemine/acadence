15cb391ee5c853ac84ef7eee7961899d3b0a3866
saturnwillbemine <lewistrejon@gmail.com>
2025-03-28 09:52:43 -0500

    added getClasses and getRosters to the API

--- src/scripts/py/db-api.py
+++ src/scripts/py/db-api.py
@@ -11,6 +11,8 @@ import bcrypt
 app = Flask(__name__)
 CORS(app, origins='http://localhost:3000') #allows for cross-origin resource sharing
 
+###################### database connection
+
 def getCursor():
     db = mysql.connect(
         host="10.101.128.56",
@@ -28,6 +30,8 @@ def closeConnection(db, cursor):
     cursor.close()
     db.close()
 
+###################### validate login
+
 @app.route("/validate", methods=['POST', 'OPTIONS']) #takes username and password and validates login
 def validate():
     if request.method == "OPTIONS":
@@ -38,10 +42,11 @@ def validate():
     return jsonify(validateLogin(data))
 
 def validateLogin(data):  # validate password in users table based on given username and password info
-    username = data.get("username")
-    password = data.get("pass")
 
     db, cursor = getCursor()
+
+    username = data.get("username")
+    password = data.get("pass")
     
     cursor.execute("SELECT pass, username, professorName, professorID FROM Professor WHERE username = %s;", (username,))
     result = cursor.fetchone()
@@ -64,6 +69,62 @@ def validateLogin(data):  # validate password in users table based on given user
     finally:
         closeConnection(db, cursor)
         return returnData
+    
+###################### get classes
+
+@app.route("/getProfClasses", methods=['POST', 'OPTIONS'])
+def getProfClasses():
+    if request.method == 'OPTIONS':
+        response = jsonify()
+        response.headers.add('Allow-Control-Allow-Headers', 'Content-Type')
+        return response
+    data = request.get_json()
+    return jsonify(getClasses(data))
+
+
+def getClasses(data):
+    db, cursor = getCursor()
+
+    profID = data.get('professorID')
+
+    cursor.execute("SELECT * FROM Classes WHERE professorID = %s;", (profID,))
+    result = cursor.fetchmany(size=8) ## maximum of 8 classes a professor could have
+
+    returnData = {}
+
+    for i in range(len(result)):
+        returnData[i] = {
+            'classID': result[i][0],
+            'professorID': result[i][1],
+            'className': result[i][2],
+            'classDept': result[i][3],
+            'classDesc': result[i][4]
+        }
+
+    return returnData
+
+###################### get class rosters
+
+@app.route("/getRoster", methods=['POST', 'OPTIONS'])
+def getRoster():
+    if request.method == 'OPTIONS':
+        response = jsonify()
+        response.headers.add('Allow-Control-Allow-Headers', 'Content-Type')
+        return response
+    data = request.get_json()
+    return jsonify(getClassRoster(data))
+
+def getClassRoster(data):
+    db, cursor = getCursor()
+
+    classID = data.get("classID")
+    studentName = data.get("studentName")
+
+
+######################
+
+
+
 
 
 app.run(host="0.0.0.0", port=5000)
