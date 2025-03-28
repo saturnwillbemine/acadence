95e7ad4d1dc85e66d7cd0cd78a29bde7d3426261
saturnwillbemine <lewistrejon@gmail.com>
2025-03-28 09:51:37 -0500

    keep login test

--- src/scripts/requestLogin.ts
+++ src/scripts/requestLogin.ts
@@ -1,23 +1,27 @@
 import { useSession } from "./userSessionStore";
 
-export default async function requestLogin(user: string, pass: string) {
+export default async function requestLogin(user: string, pass: string, keepLogin: boolean) {
     try {
         const req: Response = await fetch('http://localhost:5000/validate', {
             method: "POST",
             headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ username: user, pass: pass, })
+            body: JSON.stringify({ username: user, pass: pass, keepLogin: keepLogin})
         });
         
         const data: any = await req.json();
 
         console.log(data);
+        console.log('Login response:', data);
+        console.log('Keep logged in:', keepLogin);
         
         if (data.success) {
             useSession.getState().setSession({
               isAuthenticated: true,
               username: data.username,
               professorName: data.professorName,
-              professorId: data.professorId
+              professorId: data.professorId,
+              keepLoggedIn: keepLogin,
+              hydrated: true
             })
             return true;
           }
