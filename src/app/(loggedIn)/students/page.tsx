cfc4afc18ea321fec86fbc4e9bfcacec2268e525
saturnwillbemine <lewistrejon@gmail.com>
2025-04-03 10:51:43 -0500

    implemented student attendance into calendar

--- src/app/(loggedIn)/students/page.tsx
+++ src/app/(loggedIn)/students/page.tsx
@@ -2,20 +2,50 @@
 import StudentListSort from "@/app/components/StudentList";
 import StudentCalendar from "@/app/components/StudentCalendar";
 import { useSession } from "@/scripts/userSessionStore";
+import { useEffect, useState } from "react";
+import { UnstyledButton } from "@mantine/core";
+import requestStudentAttendance from "@/scripts/requestStudentAttendance";
 
   export default function Students() {
     const professorID = useSession((state) => state.professorID)
+    const [selection, setSelection] = useState<string>('');
+    const [attendanceData, setAttendanceData] = useState([]);
+
+    const getStudentAttendance = async () => {
+            const data = await requestStudentAttendance(selection);
+            console.log(data);
+            setAttendanceData( data || []);
+          };
+
+    useEffect(() => {
+        if (selection) {
+          getStudentAttendance();
+        } else {
+          setAttendanceData([]);
+        }
+     }, [selection]);
+
+    
 
       return (
         <div>
           <h1>Student List</h1>
           <p>This should show a selectable list of students where i can pull up their attendance data</p>
-          <div style={{ display: 'flex', flexDirection: 'row'}} >
-          <StudentListSort professorID={professorID}/>
+
+          <div style={{ display: 'flex', flexDirection: 'row'}}>
+          <StudentListSort professorID={professorID}
+                           setSelection={setSelection}
+                           selection={selection}/>
+
           <div style={{marginTop: '5%'}}>
-            <StudentCalendar/>
+            <StudentCalendar attendanceData={attendanceData}/>
           </div>
-=          </div>
+
+            <UnstyledButton onClick={() => getStudentAttendance()}> 
+              Print Selection
+            </UnstyledButton>
+
+          </div>
         </div>
       );
     }
--- src/app/components/StudentCalendar.tsx
+++ src/app/components/StudentCalendar.tsx
@@ -3,12 +3,47 @@ import { Indicator } from '@mantine/core';
 import { Calendar, DatePickerProps } from '@mantine/dates';
 import '@mantine/dates/styles.css';
 
-export default function StudentCalendar() {
+interface AttendanceData {
+  studentID: number;
+  studentName: string;
+  recordDate: string;
+  studentStatus: string;
+}
+
+interface StudentCalendarProps {
+  attendanceData: AttendanceData[];
+}
+
+const statusColors: Record<string, string> = {
+  excused: 'orange',
+  unexcused: 'red',
+  late: 'yellow',
+};
+
+export default function StudentCalendar({ attendanceData = [] }: StudentCalendarProps) {
+  const absenceDates = attendanceData.map(record =>  {
+    const date = new Date(record.recordDate);
+    date.setMinutes(date.getMinutes() + date.getTimezoneOffset());
+    
+
+    return {
+      date: date.toDateString(),
+      status: record.studentStatus.toLowerCase()
+    }
+
+  }
+  );
 
   const dateRenderer: DatePickerProps['renderDay'] = (Date) => {
-    const day = Date.getDate();
+    const day = Date.getDate()
+    const attendance = absenceDates.find(abs => abs.date === Date.toDateString()); //looking for absences dates that match the Date
+    const isAbsent = !!attendance;
+
     return(
-        <Indicator size={6} color='red' offset={-5} disabled={ day !== 1 }> 
+        <Indicator size={6} 
+        color={attendance ? statusColors[attendance.status] : 'gray'} 
+        offset={-5} 
+        disabled={ !isAbsent } > 
             <div>{day}</div>
         </Indicator>
     ); // this gives us the indicator over the day of the month for every day not in the disabled prop, if its not in the array we get back we set it to disabled
@@ -16,7 +51,7 @@ export default function StudentCalendar() {
 
   return (
     <div>
-        <Calendar maxLevel='month' minLevel='month' static renderDay={dateRenderer}/>
+        <Calendar maxLevel='month' minLevel='month' renderDay={dateRenderer}/>
     </div>
   );
 }
